93e790640a2c6bb8bac4da7bd8cdea40ac1dd1c7
akanksha123jain <akkijai145@gmail.com>
2024-01-29 15:38:46 +0530

    29th jan 2024

--- src/main/java/com/nobroker/service/OwnerPlanService.java
+++ src/main/java/com/nobroker/service/OwnerPlanService.java
@@ -2,7 +2,10 @@ package com.nobroker.service;
 
 import com.nobroker.payload.OwnerPlanDto;
 
+import java.util.List;
+
 public interface OwnerPlanService {
     public OwnerPlanDto createOwnerPlans(OwnerPlanDto ownerPlanDto);
 
+    List<OwnerPlanDto> getAllOwnerPlans();
 }
